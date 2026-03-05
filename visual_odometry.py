992ad2f6579a82f7d15362b1684c5ebdd9cf683e
Daniel Tan <daniel@macbookpro.lan>
2026-03-05 17:05:50 -0600

    Refactor: Added pose estimation into processing loop, decoupled visualization logic from VO pipeline

--- visual_odometry.py
+++ visual_odometry.py
@@ -26,9 +26,36 @@ class VisualOdometry:
         self.current_frame = None
         self.pts_ref = None # "Reference Points" (Points in old frame)
         self.pts_cur = None # "Current Points" (Points in new frame)
+        self.curr_R = np.identity(3)
+        self.curr_t = np.zeros((3,1))
         
         # Fast Detector initialization
         self.detector = cv.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
+    
+    def getAbsoluteScale(self, frame_id) -> float:
+        """
+        Calculates the Euclidean distance between the current and previous ground truth poses to provide the real-world scale.
+        
+        Args:
+            frame_id (int): The index of the current frame
+
+        Returns:
+            float: The absolute distance (scale) moved between the previous frame and current frame.
+        """
+        curr_pose = self.dataset.poses[frame_id]
+        x = curr_pose[0,3]
+        y = curr_pose[1,3]
+        z = curr_pose[2,3]
+        prev_pose = self.dataset.poses[frame_id-1]
+        x_prev = prev_pose[0,3]
+        y_prev = prev_pose[1,3]
+        z_prev = prev_pose[2,3]
+
+        if frame_id == 0:
+            return 0.0
+        else:
+            scale = np.sqrt((x-x_prev)**2 + (y-y_prev)**2 + (z-z_prev)**2)
+        return scale
 
     def featureDetection(self, img) -> np.ndarray:
         """
@@ -71,12 +98,17 @@ class VisualOdometry:
 
         return valid_pts_cur, valid_pts_ref
 
-    def processFrame(self, frame_id) -> None:
+    def processFrame(self, frame_id) -> tuple[np.ndarray, np.ndarray]:
         """
         Main Loop: Loads an image, tracks features, and draws the point.
 
         Args:
             frame_id (int): The index of the frame to process
+        
+        Returns:
+            tuple[np.ndarray, np.ndarray]: A tuple containing (current_frame, display_pts)
+                    current_frame (np.ndarray): The grayscale image of the current frame for display.
+                    pts_cur (np.ndarray): The (x, y) coordinates of the successfully tracked features in the current frame.
         """
         self.current_frame = self.dataset.get_image(frame_id)
         
@@ -85,10 +117,21 @@ class VisualOdometry:
             # Initialize: Just find points
             self.old_frame = self.current_frame
             self.pts_ref = self.featureDetection(self.current_frame)
-            return
+            return self.current_frame, self.pts_ref
 
         # Optical Flow Tracking
         self.pts_cur, self.pts_ref = self.featureTracking(self.old_frame, self.current_frame, self.pts_ref)
+
+        # Pose Estimation
+        if len(self.pts_cur) > 10:
+            E, mask = cv.findEssentialMat(self.pts_cur, self.pts_ref, self.focal_length, self.pp, method=cv.RANSAC)
+
+            _, R, t, mask = cv.recoverPose(E, self.pts_cur, self.pts_ref, focal=self.focal_length, pp=self.pp)
+
+            absolute_scale = self.getAbsoluteScale(frame_id)
+
+            self.curr_t = self.curr_t + absolute_scale * np.dot(self.curr_R, t)
+            self.curr_R = np.dot(R, self.curr_R)
 
         # Replenish Features
         # If the number of tracked points drops below 2000, detect new ones in the current frame
@@ -97,25 +140,40 @@ class VisualOdometry:
             self.pts_cur = np.concatenate((self.pts_cur,new_pts), axis=0)
             self.pts_ref = np.concatenate((self.pts_ref, new_pts), axis=0)
 
-        # Visualization
-        vis = cv.cvtColor(self.current_frame, cv.COLOR_GRAY2BGR)
-        for x, y in self.pts_cur:
-            cv.circle(vis, (int(x), int(y)), radius=2, color=(0,255,0), thickness=-1)
-        cv.imshow('V0', vis)
-        cv.waitKey(1)
-
         # Update state for next iteration
         self.old_frame = self.current_frame
         self.pts_ref = self.pts_cur
 
+        return self.current_frame, self.pts_cur
+    
+def visualize(vo_instance, frame, pts, canvas):
+    # Visualization
+    x_coord = int(vo_instance.curr_t[0,0]) + 500
+    z_coord = 500 - int(vo_instance.curr_t[2,0])
+    cv.circle(canvas, (x_coord,z_coord), 1, (0,255,0), 1)
+    cv.imshow('Trajectory', canvas)
+    
+    vis = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+    for x, y in pts:
+        cv.circle(vis, (int(x), int(y)), radius=2, color=(0,255,0), thickness=-1)
+    cv.imshow('VO Pipeline', vis)
+    cv.waitKey(1)
+
+
 if __name__ == "__main__":
     vo = VisualOdometry('data', '00')
-    
-    # Test
-    for i in range(len(vo.dataset)):
-        vo.processFrame(i)
+    traj_canvas = np.zeros((1000, 1000, 3), dtype=np.uint8)
+    total_frames = len(vo.dataset)
 
-        if i == 0:
-            print(f"Frame {i}: Initialized with {len(vo.pts_ref)} features.")
-        else:
-            print(f"Frame {i}: Detected {len(vo.pts_cur)} features.")
+    # Test highway scene
+    for i in range(1101):
+        frame, pts = vo.processFrame(i)
+        visualize(vo, frame, pts, traj_canvas)
+
+    traj_canvas = np.zeros((1000, 1000, 3), dtype=np.uint8)
+    vo.curr_R = np.identity(3)
+    vo.curr_t = np.zeros((3,1))
+    # Test neighborhood scene
+    for i in range(1101,total_frames):
+        frame, pts = vo.processFrame(i)
+        visualize(vo, frame, pts, traj_canvas)
